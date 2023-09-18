508bc15bfcd9b7318e438d60cbbf3a2978af9938
Dave Get <daveget2020@gmail.com>
2023-09-18 10:53:23 +0200

    Password Validation

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,7 +5,7 @@
 
     <label for="">Password: </label>
     <input type="password" required v-model="password" />
-
+    <div class="error" v-if="passwordError">{{ passwordError }}</div>
     <label> Role:</label>
     <select v-model="role">
       <option value="developer">developer</option>
@@ -43,6 +43,7 @@ export default {
       terms: false,
       tempskill: "",
       skills: [],
+      passwordError: "",
     };
   },
   methods: {
@@ -61,7 +62,18 @@ export default {
       });
     },
     handleSubmit() {
-      console.log("Form Submitted.");
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at leaset 6 chars long.";
+
+      if (this.password) {
+        console.log("Email", this.email);
+        console.log("Password", this.password);
+        console.log("Role", this.role);
+        console.log("skills", this.skills);
+        console.log("Terms Accepted", this.terms);
+      }
     },
   },
 };
@@ -126,5 +138,12 @@ button {
 }
 .submit {
   text-align: center;
+}
+
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
